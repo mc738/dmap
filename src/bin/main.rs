5963e6c76def9f8766990c1c501d6581aceea226
max <me@mclifford.dev>
2020-12-18 11:06:23 +0000

    Added basic arg/command handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,15 +3,50 @@ use crypto::sha2::Sha256;
 use crypto::digest::Digest;
 use std::io::{BufReader};
 use std::io::prelude::*;
-use std::io;
+use std::{io, env};
 use std::path::Path;
+use std::iter::Map;
+use crate::Command::Error;
 
+#[derive(Debug)]
+pub struct MapCommand {
+    path: String,
+    output: String
+}
+
+#[derive(Debug)]
+pub struct CommandError {
+    message: String,
+    hint: String
+}
+
+#[derive(Debug)]
+pub enum Command {
+    Map(MapCommand),
+    Error(CommandError)
+}
+
+impl MapCommand {
+    pub fn create(path: String, output: String) -> MapCommand {
+        MapCommand {
+            path,
+            output
+        }
+    }
+}
+
+impl CommandError {
+    pub fn create(message: String, hint: String) -> CommandError {
+        CommandError {
+            message,
+            hint
+        }
+    }
+}
 
 fn crawl_directory() {
     
-    // This will basically be a recursive call to `read_directory` 
-    
-    
+    // This will basically be a recursive call to `read_directory`
 }
 
 fn read_directory(path: &Path) -> io::Result<()> {
@@ -65,7 +100,6 @@ fn create_hash(mut data: Vec<u8>) -> String {
     hasher.result_str()
 }
 
-
 fn hash_file(path: &str) {
     let file = File::open(path);
 
@@ -92,9 +126,74 @@ fn hash_file(path: &str) {
     }
 }
 
+fn get_args() -> Command {
+    let args: Vec<String> = env::args().collect();
+
+    println!("Args: {:?}", args);
+    
+    let run = "run".to_string();
+    
+    let args_len = args.len();
+    
+    if args_len > 2 {
+        match &args[1] {
+            run => {
+                
+                if args_len >= 4 {
+                   Command::Map(MapCommand::create(args[2].clone(), args[3].clone()))
+                }
+                else {
+                    Command::Error(CommandError::create(
+                        "Too few args for `run` command".to_string(),
+                        "Syntax: `dmap map [path] [output]`.".to_string()))
+                }
+                
+            }
+            _ => {
+                Command::Error(CommandError::create(
+                    "Unknown command".to_string(),
+                    "Try `dmap map [path] [output]` or `dmap compare [result1] [result2]`".to_string()))
+            }
+        }  
+    }
+    else {
+        Command::Error(CommandError::create(
+            "Missing command".to_string(),
+            "Try `dmap map [path] [output]` or `dmap compare [result1] [result2]`".to_string()))
+    }
+} 
+
+fn run_command(command: Command) -> Result<(), &'static str> {
+    
+    
+    match command {
+        Command::Map(comm) => {
+            println!("Mapping directory `{}`...", comm.path);
+            
+            read_directory(comm.path.as_ref());
+        }
+        Error(error) => {
+            println!("Command error!");
+            println!("\tMessage: {}", error.message);
+            println!("\tHint: {}", error.hint);
+        }
+    };
+    
+    
+    Ok(())
+}
 
 fn main() {
-    read_directory("/home/max/Projects/dmap/".as_ref());
+    
+    let command = get_args();
+    
+    
+    println!("Command: {:?}", command);
+
+    run_command(command);
+
+
+    // read_directory("/home/max/Projects/dmap/".as_ref());
     // hash_file("/home/max/Data/HelloWorld.txt");
     // println!("Hello, world!");
 }
